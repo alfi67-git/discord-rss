08820a56d211bc413cb9f9b167299da2ca3bcc11
Alfi <85284527+alfi67-git@users.noreply.github.com>
2025-03-16 19:35:21 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,19 +3,19 @@
 # Installe tous les packages nécessaires
 sudo apt install python3.11-venv
 sudo apt install pip
-echo "Tous les packages ont été installés..."
+echo "##########  Tous les packages ont été installés..."
 
 # Créer un environnement virtuel
 python3 -m venv discord-rss
-echo "Environnement virtuel créer..."
+echo "##########  Environnement virtuel créer..."
 
 # Activer l'environnement virtuel
 source discord-rss/bin/activate
-echo "Environnement virtuel activé..."
+echo "##########  Environnement virtuel activé..."
 
 # Installer les dépendances
 pip install -r requirements.txt
-echo "Dépendances installées..."
+echo "##########  Dépendances installées..."
 
 # Exécuter le script principal
 python main.py
